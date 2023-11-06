0c4c7e7bd41bc8b3b31db517b27b9957ade80cf2
Ste <stephan.obrien@mcgill.ca>
2023-11-06 11:09:24 -0500

    Adding for range

--- Introduction/range_for.cpp
+++ Introduction/range_for.cpp
@@ -10,6 +10,32 @@ int main(){
         cout << i << endl;
     }
 
+
+    int i = 0;
+    while (true){
+
+        // Check if i is even
+        if ( i %2 == 0){
+            cout << i << " is even!" << endl;
+        }
+        else {
+            cout << i << " is odd!" << endl;
+        }
+
+        i++;
+
+        if (i > 10 ){
+            break;
+        }
+        else if (i == 3){
+            continue;
+        }
+        else {
+            cout << "Back to the start!"  << endl;
+        }
+    }
+
     return 0;
+
 
 }
